44f3e8e76e5fc810d915859be2625a617eea7782
Anthony Kishan <anthony.kishan01@gmail.com>
2025-03-14 23:20:53 +0600

    Working on route

--- app/core/Route.php
+++ app/core/Route.php
@@ -23,3 +23,53 @@ class Route
         }
     }
 }
+<?php
+# app/core/Route.php
+
+namespace App\Core;
+
+class Route
+{
+    private static $routes = [];
+
+    public static function add($url, $controller, $method = "index")
+    {
+        self::$routes[$url] = ["controller" => $controller, "method" => $method];
+    }
+
+    public static function handle($url)
+    {
+        if (isset(self::$routes[$url])) {
+            $route = self::$routes[$url];
+            $controller = new $route["controller"];
+            call_user_func([$controller, $route["method"]]);
+        } else {
+            echo "404 Not Found!";
+        }
+    }
+}
+<?php
+# app/core/Route.php
+
+namespace App\Core;
+
+class Route
+{
+    private static $routes = [];
+
+    public static function add($url, $controller, $method = "index")
+    {
+        self::$routes[$url] = ["controller" => $controller, "method" => $method];
+    }
+
+    public static function handle($url)
+    {
+        if (isset(self::$routes[$url])) {
+            $route = self::$routes[$url];
+            $controller = new $route["controller"];
+            call_user_func([$controller, $route["method"]]);
+        } else {
+            echo "404 Not Found!";
+        }
+    }
+}
